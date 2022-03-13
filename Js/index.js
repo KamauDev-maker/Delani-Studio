38c3d599bcf2b8587b8c6c0b0157be02df7c64e5
KamauDev-maker <oscarnjenga@gmail.com>
2022-03-14 00:15:30 +0300

    modifying the js

--- Js/index.js
+++ Js/index.js
@@ -1,33 +1,33 @@
 $(document).ready(function(){
-    $("#development-image").click(function(){
-      $("#development-image").slideDown('1400').hide('1000');
+    $("#development-img").click(function(){
+      $("#development-img").slideDown('1400').hide('1000');
       $("#development").show('1400');
     });
     $("#development").click(function(){
       $("#development").slideUp('1400');
-      $("#development-image").Down('1400');
+      $("#development-img").slideDown('1400');
     });
   });
 
   $(document).ready(function(){
-    $("#design-image").click(function(){
-      $("#design-image").slideDown('1400').hide('1000');
+    $("#design-img").click(function(){
+      $("#design-img").slideDown('1400').hide('1000');
       $("#design").show('1400');
     });
     $("#design").click(function(){
       $("#design").slideUp('1400');
-      $("#design-image").slideDown('1400');
+      $("#design-img").slideDown('1400');
     });
   });
   
   $(document).ready(function(){
-    $("#product-image").click(function(){
-      $("#product-image").slideDown('1400').hide('1000');
+    $("#product-img").click(function(){
+      $("#product-img").slideDown('1400').hide('1000');
       $("#product").show('1400');
     });
     $("#product").click(function(){
       $("#product").slideUp('1400');
-      $("#product-image").slideDown('1400');
+      $("#product-img").slideDown('1400');
     });
   });
   
